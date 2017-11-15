fb4ecc2c29d49d7a6f77b107339025738ec3e9bc
Sami Lindqvist <sami.lindqvist@helsinki.fi>
2017-11-16 00:13:19 +0200

    1-3 done

---
+++ viikko3/Kurssistatistiikka/src/main/java/ohtu/Course.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu;
+
+/**
+ *
+ * @author saklindq
+ */
+public class Course {
+
+    private String name;
+    private String term;
+    private int[] exercises;
+
+    public Course() {
+    }
+
+    public int[] getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(int[] exercises) {
+        this.exercises = exercises;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+}
--- viikko3/Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ viikko3/Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -1,32 +1,60 @@
 package ohtu;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.io.IOException;
+
 import org.apache.http.client.fluent.Request;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         // vaihda oma opiskelijanumerosi seuraavaan, ÄLÄ kuitenkaan laita githubiin omaa opiskelijanumeroasi
-        String studentNr = "11";
+        String studentNr = "011120775";
         if (args.length > 0) {
             studentNr = args[0];
         }
 
         String url = "https://studies.cs.helsinki.fi/ohtustats/students/" + studentNr + "/submissions";
+        String courseUrl = "https://studies.cs.helsinki.fi/ohtustats/courseinfo";
+        String statsUrl = "https://studies.cs.helsinki.fi/ohtustats/stats";
 
         String bodyText = Request.Get(url).execute().returnContent().asString();
-
-        System.out.println("json-muotoinen data:");
-        System.out.println(bodyText);
+        String courseBodyText = Request.Get(courseUrl).execute().returnContent().asString();
+        String statsBodyText = Request.Get(statsUrl).execute().returnContent().asString();
 
         Gson mapper = new Gson();
+        JsonParser parser = new JsonParser();
+
         Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+        Course crs = mapper.fromJson(courseBodyText, Course.class);
+        JsonObject statsObj = parser.parse(statsBodyText).getAsJsonObject();
 
-        System.out.println("Oliot:");
+        System.out.println("Kurssi: " + crs.getName() + ", " + crs.getTerm() + "\n");
+        System.out.println("opiskelijanumero: " + studentNr + "\n");
         for (Submission submission : subs) {
-            System.out.println(submission);
+            String done = "";
+            for (int i = 0; i < submission.getExercises().length; i++) {
+                done += submission.getExercises()[i];
+                done += " ";
+            }
+            System.out.println("viikko " + submission.getWeek() + ":");
+            System.out.println(" tehtyjä tehtäviä yhteensä: " + submission.getExercises().length + " (maksimi " + crs.getExercises()[submission.getWeek() - 1] + "), aikaa kului " + submission.getHours() + " tuntia, tehdyt tehtävät: " + done);
         }
-
+        int done = 0;
+        int tunnit = 0;
+        for (int i = 0; i < subs.length; i++) {
+            done += subs[i].getExercises().length;
+            tunnit += subs[i].getHours();
+        }
+        System.out.println("\n yhteensä: " + done + " tehtävää " + tunnit + " tuntia");
+        int courseHours = 0;
+        int courseExercises = 0;
+        for (int i = 0; i < statsObj.size(); i++) {
+            courseHours += statsObj.get((i + 1) + "").getAsJsonObject().get("hour_total").getAsInt();
+            courseExercises += statsObj.get((i + 1) + "").getAsJsonObject().get("exercise_total").getAsInt();
+        }
+        System.out.println("\nKaikkien kurssilaisten tehtäväpalautuksia: " + courseExercises + " sekä käytetyt tunnit:  " + courseHours);
     }
 }
