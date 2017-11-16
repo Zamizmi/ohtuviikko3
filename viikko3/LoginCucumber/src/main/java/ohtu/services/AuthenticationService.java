1d221557031a305df04be66736498b896eed1ee1
Sami Lindqvist <sami.lindqvist@helsinki.fi>
2017-11-16 19:41:03 +0200

    7 done

--- viikko3/LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
+++ viikko3/LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
@@ -39,8 +39,11 @@ public class AuthenticationService {
     }
 
     private boolean invalid(String username, String password) {
-        // validity check of username and password
-
-        return false;
+        if (username.matches("^[A-Za-z]{3,}$")) {
+            if (password.matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d$@$!%*#?&]{8,}$")) {
+                return false;
+            }
+        }
+        return true;
     }
 }
